94fb76a7964d4879864ea040f9c80f27bb809a8b
Cole Hartog <agentcole7@gmail.com>
2016-01-04 17:22:01 -0700

    did stuff and things

--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -3,5 +3,47 @@ var app = angular.module('nbaRoutes');
 app.service('teamService', function ($http, $q) {
 
     // service code
+    this.addNewGame = function(gameObject){
+        var url = 'https://api.parse.com/1/classes/' + gameObject.homeTeam;
+        if(parseInt(gameObject.homeTeamScore) > parseInt(gameObject.opponentScore)){
+            gameObject.won = true;
+        }
+        else{
+            gameObject.won = false;
+        }
+        
+        return $http.post({
+            url: url,
+            data: {gameObject}
+        }).success(function(stuff){console.log('yay')})
+    }
+    
+    this.getTeamData = function(team){
+        var deffered = $q.defer();
+        
+        var url = 'https://api.parse.com/1/classes/' + team;
+        
+        $http.get(url).then(function(data){
+            var results = data.data.results;
+            var wins = 0;
+            var losses = 0;
+            for(var i = 0; i < results.length; i++){
+                if(results[i].won == true){
+                    wins++;
+                }
+                else{
+                    losses++;
+                }
+            }
+            results.wins = wins;
+            results.losses = losses;
+            
+            deffered.resolve(results);
+            console.log(results[0]);
+        })
+        
+        return deffered.promise;
+    }
+    
 
 });
